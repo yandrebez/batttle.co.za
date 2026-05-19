31b4fe2f92a4769ff3c879230fe14ad32ccae594
yandrebez <yandrebez007@gmail.com>
2026-05-19 15:12:49 +0200

    Use Cinzel font for MORE on maintenance page

--- components/MaintenancePage.tsx
+++ components/MaintenancePage.tsx
@@ -1,11 +1,17 @@
+import { Cinzel } from "next/font/google";
 import styles from "./MaintenancePage.module.css";
+
+const cinzel = Cinzel({
+  subsets: ["latin"],
+  weight: ["700"],
+});
 
 export function MaintenancePage() {
   return (
     <main className={styles.maintenance}>
       <div className={styles.content}>
         <div className={styles.title}>Are you ready for</div>
-        <div className={styles.more}>MORE</div>
+        <div className={`${styles.more} ${cinzel.className}`}>MORE</div>
       </div>
     </main>
   );
